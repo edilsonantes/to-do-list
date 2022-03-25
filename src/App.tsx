9a97fa490e299ab1061f6ec42a714bd5372d3d39
edilson <nantes.junior@gmail.com>
2022-03-24 23:19:06 -0300

    Criaçao do card das tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import { useState } from "react"
-import { Area, Container, Header } from "./App.styles"
+import { useState } from "react";
+import { Area, Container, Header } from "./App.styles";
+import { ListItem } from "./components/ListItem";
 import {Item} from "./types/item";
 
+
 function App() {
-  const [list, setList] = useState<Item[]>([]);
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: "Comprar pão", done:true} 
+  ]);
 
   return (
     <Container>
       <Area>
         <Header>Lista de Tarefas</Header>
+
+      {/*Lugar que vai adicionar tarefas */}
+
+      {list.map((item, index)=> (
+        <ListItem key = {index} item = {item}/>
+      ))}
+
       </Area>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/types/item.ts
+++ src/types/item.ts
@@ -1,5 +1,5 @@
 export type Item = {
     id: number;
     name: string;
-    status: boolean;
+    done: boolean;
 }
